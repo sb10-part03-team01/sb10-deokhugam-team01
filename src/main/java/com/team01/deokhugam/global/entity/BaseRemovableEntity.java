d2542244249526c3529be742a4418c6a0e089aa2
jongin-git <jongin33@gmail.com>
2026-04-15 11:03:32 +0900

    feat: Base*Entity 생성

---
+++ src/main/java/com/team01/deokhugam/global/entity/BaseRemovableEntity.java
@@ -0,0 +1,24 @@
+package com.team01.deokhugam.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseRemovableEntity extends BaseUpdatableEntity {
+
+  @Column(name = "is_deleted", nullable = false)
+  private boolean isDeleted = false;
+
+  @Column(name = "deleted_at")
+  private Instant deletedAt;
+
+  // 논리 삭제 용 -> 삭제여부 O / 삭제시간 부여
+  // 물리적 삭제는 테스트만
+  public void softDelete() {
+    this.isDeleted = true;
+    this.deletedAt = Instant.now();
+  }
+}
