c7e45a077c3572a506fcea10d5f86289fabe690f
zelote123 <fredsonata@gmail.com>
2026-04-15 14:22:29 +0900

    fix: 오타 수정

--- src/main/java/com/team01/deokhugam/global/exception/ErrorCode.java
+++ src/main/java/com/team01/deokhugam/global/exception/ErrorCode.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum  ErrorCode {
-  BOOK_NOT_FOUND(404, "BOOK_NOT_FOUND", "도서를 찾을 없습니다");
+  BOOK_NOT_FOUND(404, "BOOK_NOT_FOUND", "도서를 찾을 수 없습니다");
 
   private final int status;
   private final String code;
