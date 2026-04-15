9e86c6f95df5268f5e029f415dc3f06e4ab0e401
jongin-git <jongin33@gmail.com>
2026-04-15 11:11:54 +0900

    fix: 원래 delete시간 유지하도록 추가

--- src/main/java/com/team01/deokhugam/global/entity/BaseRemovableEntity.java
+++ src/main/java/com/team01/deokhugam/global/entity/BaseRemovableEntity.java
@@ -18,6 +18,9 @@ public abstract class BaseRemovableEntity extends BaseUpdatableEntity {
   // 논리 삭제 용 -> 삭제여부 O / 삭제시간 부여
   // 물리적 삭제는 테스트만
   public void softDelete() {
+    if (this.isDeleted) {
+      return;
+    }
     this.isDeleted = true;
     this.deletedAt = Instant.now();
   }
