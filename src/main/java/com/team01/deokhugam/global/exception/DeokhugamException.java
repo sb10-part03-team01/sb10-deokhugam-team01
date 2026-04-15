f4a8c3c866d5f0b0ae99731cb50b198b96e755ac
zelote123 <fredsonata@gmail.com>
2026-04-15 14:34:16 +0900

    fix: 오타 수정

--- src/main/java/com/team01/deokhugam/global/exception/DeokhugamException.java
+++ src/main/java/com/team01/deokhugam/global/exception/DeokhugamException.java
@@ -1,5 +1,6 @@
 package com.team01.deokhugam.global.exception;
 
+import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.Map;
 import lombok.Getter;
@@ -8,11 +9,11 @@ import lombok.Getter;
 public class DeokhugamException extends RuntimeException{
   private final ErrorCode errorCode;
   private final Map<String, Object> details;
-  private final Instant timestamp;
+  private final Instant timeStamp;
 
   public DeokhugamException(ErrorCode errorCode, Map<String, Object> details){
     this.errorCode = errorCode;
     this.details = details;
-    this.timestamp = Instant.now();
+    this.timeStamp = Instant.now();
   }
 }
