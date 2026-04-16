b13fdb055a3a1cd93d09c37526072a254d73d378
안승리 <hauall@naver.com>
2026-04-16 15:37:04 +0900

    알림관리 레포지토리 수정2

--- src/main/java/com/team01/deokhugam/notification/repository/NotificationRepository.java
+++ src/main/java/com/team01/deokhugam/notification/repository/NotificationRepository.java
@@ -2,6 +2,7 @@ package com.team01.deokhugam.notification.repository;
 
 import com.team01.deokhugam.notification.entity.Notification;
 import java.time.Instant;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +12,6 @@ public interface NotificationRepository extends JpaRepository<Notification, UUID
 
   Page<Notification> findAllByUserId(UUID userId, Pageable pageable);
 
-  void deleteAllByIsReadTrueAndUpdatedAtBefore(Instant cutoff);
+  void deleteAllByIsReadTrueAndUpdatedAtBefore(OffsetDateTime cutoff);
 
 }
