ebe381d1c7d95792395ce9417843b87e71e8a519
안승리 <hauall@naver.com>
2026-04-16 15:19:43 +0900

    알림관리 레포지토리 수정

--- src/main/java/com/team01/deokhugam/notification/repository/NotificationRepository.java
+++ src/main/java/com/team01/deokhugam/notification/repository/NotificationRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface notificationRepository extends JpaRepository<Notification, UUID> {
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 
   Page<Notification> findAllByUserId(UUID userId, Pageable pageable);
 
