2b5dd096f9e5bbbb283eae30df1df9bfc311ccd0
raonPsm <s102075@naver.com>
2026-04-16 14:45:50 +0900

    WIP: merge 전 문제 해결

---
+++ src/main/java/com/team01/deokhugam/global/exception/user/EmailAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.team01.deokhugam.global.exception.user;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+
+  public EmailAlreadyExistsException(String message) {
+    super(message);
+  }
+}
