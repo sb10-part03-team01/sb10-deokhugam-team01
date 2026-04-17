58a4531357d5427589d97d85f72ee5356558a2a2
raonPsm <s102075@naver.com>
2026-04-17 13:29:23 +0900

    refactor: UserException 추상 클래스로 수정
    - UserException은 상속을 전제로 한 도메인 기반 예외이므로 직접 인스턴스화되지 않도록 abstract class로 선언

--- src/main/java/com/team01/deokhugam/global/exception/user/UserException.java
+++ src/main/java/com/team01/deokhugam/global/exception/user/UserException.java
@@ -4,8 +4,8 @@ import com.team01.deokhugam.global.exception.DeokhugamException;
 import com.team01.deokhugam.global.exception.ErrorCode;
 import java.util.Map;
 
-public class UserException extends DeokhugamException {
-  
+public abstract class UserException extends DeokhugamException {
+
   public UserException(ErrorCode errorCode, Map<String, Object> details) {
     super(errorCode, details);
   }
