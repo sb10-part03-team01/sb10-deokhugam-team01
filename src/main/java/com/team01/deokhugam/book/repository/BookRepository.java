f096d7de338d7fd332048c1cc047db2d57e36bb0
zelote123 <fredsonata@gmail.com>
2026-04-17 17:49:51 +0900

    refactor: querydsl을 상속함

--- src/main/java/com/team01/deokhugam/book/repository/BookRepository.java
+++ src/main/java/com/team01/deokhugam/book/repository/BookRepository.java
@@ -1,12 +1,13 @@
-package com.team01.deokhugam.book;
+package com.team01.deokhugam.book.repository;
 
+import com.team01.deokhugam.book.Book;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, UUID> {
+public interface BookRepository extends JpaRepository<Book, UUID>, BookRepositoryQueryDsl {
   boolean existsByIsbn(String isbn);
 
   Optional<Book> findByIdAndIsDeletedFalse(UUID id);
