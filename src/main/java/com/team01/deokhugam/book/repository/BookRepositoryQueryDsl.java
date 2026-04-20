eef759654512c6d520942b48eda4c3cf791120da
zelote123 <fredsonata@gmail.com>
2026-04-20 16:33:23 +0900

    refactor: 정렬방향 enum타입으로 변경

--- src/main/java/com/team01/deokhugam/book/repository/BookRepositoryQueryDsl.java
+++ src/main/java/com/team01/deokhugam/book/repository/BookRepositoryQueryDsl.java
@@ -1,11 +1,12 @@
 package com.team01.deokhugam.book.repository;
 
 import com.team01.deokhugam.book.entity.Book;
+import com.team01.deokhugam.global.enums.SortDirection;
 import java.time.OffsetDateTime;
 import java.util.List;
 
 public interface BookRepositoryQueryDsl {
-  List<Book> findBooks(String keyword, String orderBy, String direction, String cursor, OffsetDateTime after, int limit);
+  List<Book> findBooks(String keyword, String orderBy, SortDirection direction, String cursor, OffsetDateTime after, int limit);
 
   long countBooks(String keyword);
 
