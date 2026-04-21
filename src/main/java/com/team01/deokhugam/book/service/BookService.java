f3d2ecec39e8e3295cbd62f13dc0c393b3381c96
zelote123 <fredsonata@gmail.com>
2026-04-21 14:37:37 +0900

    refactor: 테스트 코드에 restclient.builder mock 주입

--- src/main/java/com/team01/deokhugam/book/service/BookService.java
+++ src/main/java/com/team01/deokhugam/book/service/BookService.java
@@ -22,7 +22,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import java.util.function.Function;
-import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
--- src/test/java/com/team01/deokhugam/book/BookServiceTest.java
+++ src/test/java/com/team01/deokhugam/book/BookServiceTest.java
@@ -34,6 +34,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestClient;
 
 @ExtendWith(MockitoExtension.class)
 class BookServiceTest {
@@ -43,6 +44,9 @@ class BookServiceTest {
 
   @Mock
   private BookMapper bookMapper;
+
+  @Mock
+  private RestClient.Builder restClientBuilder;
 
   @InjectMocks
   private BookService bookService;
