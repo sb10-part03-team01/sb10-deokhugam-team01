57610d53c99c4f50e6ada66111cedf5daa657b98
zelote123 <fredsonata@gmail.com>
2026-04-21 18:55:11 +0900

    fix: ci 통과를 위한 설정 추가

--- src/test/java/com/team01/deokhugam/DeokhugamApplicationTests.java
+++ src/test/java/com/team01/deokhugam/DeokhugamApplicationTests.java
@@ -3,7 +3,10 @@ package com.team01.deokhugam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "naver.client.id=test-id",
+    "naver.client.secret=test-secret"
+})
 class DeokhugamApplicationTests {
 
 	@Test
