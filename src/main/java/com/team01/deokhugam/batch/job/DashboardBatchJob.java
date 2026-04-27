5e5bb0c3d01f676624e3d2d453dbc69e08e7a954
안승리 <hauall@naver.com>
2026-04-27 11:35:35 +0900

    feat: 대시보드배치잡 구현

--- src/main/java/com/team01/deokhugam/batch/job/DashboardBatchJob.java
+++ src/main/java/com/team01/deokhugam/batch/job/DashboardBatchJob.java
@@ -1,5 +1,23 @@
 package com.team01.deokhugam.batch.job;
 
+import com.team01.deokhugam.batch.common.DashboardPeriod;
+import com.team01.deokhugam.batch.service.DashboardBatchService;
+import com.team01.deokhugam.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class DashboardBatchJob {
+
+  private final DashboardBatchService dashboardBatchService;
+  private final NotificationService notificationService;
+
+  @Scheduled(cron = "0 0 0 * * *")
+  public void run() {
+    dashboardBatchService.calculatePowerUserRanking(DashboardPeriod.today());
+    notificationService.cleanupReadNotifications();
+  }
 
 }
