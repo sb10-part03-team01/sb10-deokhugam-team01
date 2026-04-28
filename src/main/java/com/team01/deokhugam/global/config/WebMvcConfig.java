39e314db88c7711f463aba90760339fd49a8400f
zelote123 <fredsonata@gmail.com>
2026-04-28 10:02:12 +0900

    fix:코드 레빗 조건 반영
    - 로컬 저장소일때만 해당 설정파일 사용하도록 함

--- src/main/java/com/team01/deokhugam/global/config/WebMvcConfig.java
+++ src/main/java/com/team01/deokhugam/global/config/WebMvcConfig.java
@@ -2,11 +2,13 @@ package com.team01.deokhugam.global.config;
 
 import java.nio.file.Paths;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ConditionalOnProperty(name = "deokhugam.storage.type", havingValue = "local", matchIfMissing = true)
 public class WebMvcConfig implements WebMvcConfigurer {
 
   @Value("${deokhugam.storage.local.root-path}")
